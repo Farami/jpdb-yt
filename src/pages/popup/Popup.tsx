814ca75dae0fd08779aa4e46317faf39c08a68ef
Farami <niclas@farami.de>
2023-03-07 23:07:25 +0100

    feat: improve popup style

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,38 +1,51 @@
 import React, { useState } from "react";
 
-import "@pages/popup/Popup.css";
 import useAuthStore from "@src/store/auth";
 import useAxios from "@src/hooks/useAxios";
 
 const Popup = () => {
-	const axios = useAxios();
-	const [settings, setSettings] = useAuthStore();
+  const axios = useAxios();
+  const [settings, setSettings] = useAuthStore();
 
-	const [testState, setTestState] = useState("");
+  const [testState, setTestState] = useState("");
 
-	const testToken = async () => {
-		const response = await axios.get("ping");
-		const status = response.status;
+  const testToken = async () => {
+    const response = await axios.get("ping");
+    const status = response.status;
 
-		setTestState(status === 200 ? "success" : "failed");
-	};
+    setTestState(status === 200 ? "success" : "failed");
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<label>jpdb.io API token</label>
-				<input
-					value={settings.token}
-					onChange={(ev) => setSettings({ token: ev.target.value })}
-				/>
+    setTimeout(() => setTestState(""), 2000);
+  };
 
-				<button type="button" onClick={testToken}>
-					Test token
-				</button>
-				<span>{testState}</span>
-			</header>
-		</div>
-	);
+  return (
+    <div className="flex flex-col items-center justify-center py-2 px-6 w-80 dark:bg-neutral-800">
+      <header className="font-bold text-lg pb-6 dark:text-white">
+        jpdb.io youtube subtitle parser
+      </header>
+      <label
+        htmlFor="token"
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+      >
+        API token
+      </label>
+      <input
+        id="token"
+        className="border text-sm rounded-lg block w-full p-1 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+        value={settings.token}
+        onChange={(ev) => setSettings({ token: ev.target.value })}
+      />
+
+      <button
+        type="button"
+        onClick={testToken}
+        className="text-white border bg-gray-700 border-gray-600 w-full p-2 my-2"
+      >
+        Test token
+      </button>
+      <span className="text-white text-md">{testState}</span>
+    </div>
+  );
 };
 
 export default Popup;
--- vite.config.ts
+++ vite.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
 		rollupOptions: {
 			input: {
 				content: resolve(pagesDir, "content", "index.ts"),
-				contentStyle: resolve(pagesDir, "content", "style.scss"),
+				contentStyle: resolve(pagesDir, "content", "style.css"),
 				popup: resolve(pagesDir, "popup", "index.html"),
 			},
 			watch: {
